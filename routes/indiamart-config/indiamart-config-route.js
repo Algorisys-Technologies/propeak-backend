7b091d16fe2b9c0ca3d1e35ad809580651fd2b84
zoyajahidshaikh <zoya.shaikh@algorisys.com>
2024-12-10 13:35:57 +0530

    update

--- routes/indiamart-config/indiamart-config-route.js
+++ routes/indiamart-config/indiamart-config-route.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const indiaMartLeadsController = require("../../controllers/indiamart-config/indiamart-config-controller");
 
-router.post("/leads", indiaMartLeadsController.getIndiaMARTLeads);
 router.post("/lead", indiaMartLeadsController.getLeads);
 
 module.exports = router;
