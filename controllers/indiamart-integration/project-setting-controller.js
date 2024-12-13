fd4cca9f123d844218822aeb94764cef54189fb5
Rushikesh <rushikesh.belkunde@algorisys.com>
2024-12-13 16:54:56 +0530

    fix : toast message

--- controllers/indiamart-integration/project-setting-controller.js
+++ controllers/indiamart-integration/project-setting-controller.js
@@ -221,12 +221,12 @@ exports.fetchIndiaMartSettings = async (req, res) => {
         }
       }
 
-      res.status(200).json({ message: "Process completed successfully." });
+      res.status(200).json({ success: true,  message: "Leads fetched successfully." });
     } catch (error) {
       console.error("Error fetching IndiaMart settings: ", error);
-      res.status(500).json({ message: "Internal server error." });
+      res.status(500).json({ success: false,  message: "Internal server error." });
     }
   } else {
-    res.status(404).json({ message: "Project settings not found." });
+    res.status(404).json({ success: false,  message: "Project settings not found." });
   }
 };
