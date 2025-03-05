05a4ddfcb029d3cab1b51d963748a8337af42038
algo-rohit-kumbhar <rohit.kumbhar@algorisys.com>
2025-03-05 10:32:16 +0530

    updated isDeleted

--- controllers/task/task-controller.js
+++ controllers/task/task-controller.js
@@ -2342,7 +2342,7 @@ exports.getTasksStagesByProjectId = async (req, res) => {
     const taskStages = await TaskStage.find({
       title: { $in: taskStagesTitles },
       companyId: companyId,
-      isDeleted: false,
+      $or: [{ isDeleted: false }, { isDeleted: { $exists: false } }]
     }).sort({ sequence: "asc" });
     return res.json({ success: true, taskStages, project });
   } catch (error) {
