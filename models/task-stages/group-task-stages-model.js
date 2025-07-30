264f1fdb6277ea9376149cb884450ece163baef4
Madhuri <madhuri.bansode@algorisys.com>
2025-07-30 13:53:10 +0530

    Implemented group-level task stage functionality

---
+++ models/task-stages/group-task-stages-model.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+// Define the database model for task stages
+const GroupTaskStageSchema = new mongoose.Schema(
+  {
+    sequence: {
+      type: Number,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    displayName: {
+      type: String,
+      required: true,
+    },
+    show: {
+      type: Boolean,
+      default: true,
+    },
+    companyId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Company",
+      required: true,
+    },
+    groupId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "GroupMaster",
+      required: true,
+    },
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { versionKey: false }
+);
+GroupTaskStageSchema.index({ companyId: 1 });
+
+const GroupTaskStage = mongoose.model("groupTaskStage", GroupTaskStageSchema);
+module.exports = GroupTaskStage;
