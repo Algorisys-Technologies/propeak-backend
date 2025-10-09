3c94d0ca93c8852d26ecdb4e82558416455f89e7
Madhuri <madhuri.bansode@algorisys.com>
2025-10-09 18:59:25 +0530

    feat: add group-level email config with create, get, update; Added encryption/decryption for passwords;

---
+++ models/email-config/group-email-config-model.js
@@ -0,0 +1,55 @@
+const mongoose = require("mongoose");
+
+const GroupEmailConfigSchema = new mongoose.Schema(
+  {
+    companyId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "company",
+      required: true,
+    },
+    groupId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "group",
+      required: true,
+    },
+    projectStageId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "projectStage",
+      required: true,
+    },
+    authentication: [
+      {
+        username: { type: String, required: true },
+        password: { type: String, required: true },
+      },
+    ],
+    smtpSettings: {
+      host: { type: String, required: true },
+      port: { type: Number, required: true },
+      tls: { type: Boolean, default: true },
+    },
+    emailPatterns: [
+      {
+        pattern_name: { type: String, required: true },
+        subject: { type: String, required: true },
+        body_contains: { type: String, required: true },
+        from: { type: String, required: true },
+        priority: {
+          type: String,
+          enum: ["low", "medium", "high"],
+          default: "medium",
+        },
+      },
+    ],
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+    },
+    lastFetched: { type: Date },
+    lastToFetched: { type: Date },
+    isDeleted: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("groupEmailConfig", GroupEmailConfigSchema);
